308e9a285621feb9103be4f827ad58360194641d
thayanenns <thayanenns@gmail.com>
2025-01-24 23:13:45 -0300

    Tempo de simulação adicionado

--- envio/src/main/java/acc/br/techflow/envio/listener/EnvioListener.java
+++ envio/src/main/java/acc/br/techflow/envio/listener/EnvioListener.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @Component
 public class EnvioListener {
@@ -18,19 +19,34 @@ public class EnvioListener {
 
     @RabbitListener(queues = {"oito.fl.envio.retirada-aprovada"})
     public void consomePedidos(PedidoRabbitMQDTO pedidoRabbitMQDTO) {
-        atualizarStatusPedido(pedidoRabbitMQDTO);
+        if (simularEnvioPedido()) {
+            atualizarStatusPedido(pedidoRabbitMQDTO, StatusPedidoEnum.ENVIADO);  // Status ENVIADO se o envio foi bem-sucedido
+        } else {
+            atualizarStatusPedido(pedidoRabbitMQDTO, StatusPedidoEnum.FALHA_ENVIO);  // Status FALHA_ENVIO se o envio falhou
+        }
     }
 
     //para produzir mensagens
-    private void atualizarStatusPedido(PedidoRabbitMQDTO pedidoRabbitMQDTO) {
-        StatusPedidoEnum novoStatus = StatusPedidoEnum.ENVIADO;
-
+    private void atualizarStatusPedido(PedidoRabbitMQDTO pedidoRabbitMQDTO, StatusPedidoEnum novoStatus) {
         StatusPedidoRabbitMQDTO statusPedidoDTO = new StatusPedidoRabbitMQDTO(
                 pedidoRabbitMQDTO.getPedidoId(),
                 LocalDateTime.now(),
                 novoStatus
         );
         //produz mensagem para a exchange oito.ex.status-pedido
-       enviarMensagemRabbitMQService.enviarMensagem(statusPedidoDTO);
+        enviarMensagemRabbitMQService.enviarMensagem(statusPedidoDTO);
+    }
+
+    private boolean simularEnvioPedido() {
+        try {
+            // Simulação do tempo de processamento de envio
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(10);
+
+        return numeroAleatorio < 7;
     }
 }
