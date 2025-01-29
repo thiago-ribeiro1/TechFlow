5d5fdcd9891464d9401c7afd07d7c5bbe718dbb7
vagner.barbosa <vagner.barbosa@cwi.com.br>
2025-01-29 19:57:33 -0300

    Unifiquei o swagger de todos os serviços pela API Gateway

---
+++ pedido/src/main/java/acc/br/techflow/pedido/config/cors/CorsConfig.java
@@ -0,0 +1,27 @@
+package acc.br.techflow.pedido.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:8086"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
